381f976e80cb5aef822ea7971f4222e03e969337
lalarcon-py <74345096+lalarcon-py@users.noreply.github.com>
2021-04-28 15:54:47 -0700

    Update main.py
    Added death recognition, this requires OpenCV to be installed in the Virtual Environment

--- main.py
+++ main.py
@@ -6,26 +6,29 @@ def main():
     pyautogui.FAILSAFE = True
     #Timer
     print("Starting", end='')
-    for i in range(0,10):
+    for i in range(0,5):
         print(".", end='')
         time.sleep(1)
 
-    print('Go')
+    print(' Go')
     Skill4 = pyautogui.locateOnScreen('Skill4.png')
     Skill3 = pyautogui.locateOnScreen('Skill3.png')
     Skill2 = pyautogui.locateOnScreen('Skill2.png')
-    Death = pyautogui.locateOnScreen('DeathScrren.png')
+    Death = pyautogui.locateOnScreen('DeathScrren.png', confidence=1)
     #Do Something
     pyautogui.leftClick()
     time.sleep(1)
     pyautogui.leftClick()
-    while True:
+    while Death == None:
         pyautogui.press('4')
         time.sleep(1)
         pyautogui.press('3')
         time.sleep(1)
         pyautogui.press('2')
         time.sleep(1)
+    if Death != None:
+        time.sleep(10)
+
 
     print("Done.")
 if __name__ == "__main__":
