5304c40fcf05849413b20deaab64061fbcaa7fb0
lalarcon-py <74345096+lalarcon-py@users.noreply.github.com>
2021-05-01 10:10:17 -0700

    Update main.py
    Implemented multi threading behavior as to heal your character when low (FUTURE update)

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pyautogui
 import time
+import concurrent.futures
+
 
 def main():
-
     pyautogui.FAILSAFE = True
     #Timer
     print("Starting", end='')
@@ -10,26 +11,42 @@ def main():
         print(".", end='')
         time.sleep(1)
 
-    print(' Go')
-    Skill4 = pyautogui.locateOnScreen('Skill4.png')
-    Skill3 = pyautogui.locateOnScreen('Skill3.png')
-    Skill2 = pyautogui.locateOnScreen('Skill2.png')
-    Death = pyautogui.locateOnScreen('DeathScrren.png', confidence=1)
-    #Do Something
+    print('\nGo')
+    #Variables
+    Skill4 = pyautogui.locateOnScreen('Skill4.png', confidence=0.7)
+    Skill3 = pyautogui.locateOnScreen('Skill3.png', confidence=0.7)
+    Skill2 = pyautogui.locateOnScreen('Skill2.png', confidence=0.7)
+    Death = pyautogui.locateOnScreen('DeathScrren.png', confidence=0.6)
+    In_Combat = pyautogui.locateOnScreen('In_Combat.png', confidence=1)
+    HP = pyautogui.locateOnScreen('HP.png', confidence=0.2)
+    Mana = pyautogui.locateOnScreen('Mana.png', confidence=1)
+    Rest = pyautogui.locateOnScreen('Rest.png', confidence=1)
+
+    #Move
     pyautogui.leftClick()
     time.sleep(1)
-    pyautogui.leftClick()
-    while Death == None:
-        pyautogui.press('4')
-        time.sleep(1)
-        pyautogui.press('3')
-        time.sleep(1)
-        pyautogui.press('2')
-        time.sleep(1)
-    if Death != None:
-        time.sleep(10)
 
+    #Leveling Script
+    for i in range(0,90):
+        print(".", end='')
+        pyautogui.click(Rest)
+        time.sleep(24)
+        while Death == None:
+            if Skill4 != None:
+                pyautogui.press('4')
+            time.sleep(2)
+            if Skill3 != None:
+                pyautogui.press('3')
+            time.sleep(2)
+            if Skill2 != None:
+                pyautogui.press('2')
+            time.sleep(2)
+        if Death != None:
+            time.sleep(15)
 
     print("Done.")
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    f1 = executor.submit(main)
 if __name__ == "__main__":
     main()
