f8f4199ed3d91b77b5bc926a80afa195d95304ff
lalarcon-py <74345096+lalarcon-py@users.noreply.github.com>
2021-05-05 11:58:48 -0700

    Update main.py
    Switched to multiprocessing rather than multi threading due to Global Lock in Python.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import os
 import pyautogui
 import time
-import concurrent.futures
+from multiprocessing import Process
 
 
 def main():
@@ -27,26 +28,41 @@ def main():
     time.sleep(1)
 
     #Leveling Script
-    for i in range(0,90):
-        print(".", end='')
-        pyautogui.click(Rest)
-        time.sleep(24)
-        while Death == None:
-            if Skill4 != None:
-                pyautogui.press('4')
-            time.sleep(2)
-            if Skill3 != None:
-                pyautogui.press('3')
-            time.sleep(2)
-            if Skill2 != None:
-                pyautogui.press('2')
-            time.sleep(2)
-        if Death != None:
-            time.sleep(15)
+    while Death == None:
+        if Skill4 != None:
+            pyautogui.press('4')
+        time.sleep(2)
+        if Skill3 != None:
+            pyautogui.press('3')
+        time.sleep(2)
+        if Skill2 != None:
+            pyautogui.press('2')
+        time.sleep(2)
+    if Death != None:
+        time.sleep(15)
 
     print("Done.")
 
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    f1 = executor.submit(main)
+def healing():
+    In_Combat = pyautogui.locateOnScreen('In_Combat.png', confidence=0.5)
+    HP = pyautogui.locateOnScreen('HP.png', confidence=0.2)
+    Mana = pyautogui.locateOnScreen('Mana.png', confidence=1)
+    Rest = pyautogui.locateOnScreen('Rest.png', confidence=1)
+
+    while In_Combat != None:
+        time.sleep(10)
+        pyautogui.typewrite('/rest')
+        pyautogui.press('Enter')
+        time.sleep(10)
+processes = []
+for i in range(os.cpu_count()):
+    print('Registering process')
+    processes.append(Process(target=main))
+
+for process in processes:
+    processes.start()
+
+for process in processes:
+    processes.join()
 if __name__ == "__main__":
     main()
